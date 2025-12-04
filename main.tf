26fa52628858e9ad49ce1717fb39c49b6c95055a
Sujay Samanta <sujay.samanta@hashicorp.com>
2025-12-04 09:05:22 +0530

    Update subnet_id in EC2 instance module configuration

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "ec2_instance" {
   ami                    = var.ami_id
   monitoring             = true
   vpc_security_group_ids = [] # Open to world or specific SG if needed, keeping empty for basic example
-  subnet_id              = "" # Optional: specify if needed, otherwise default VPC is often used or it might fail if no default VPC. For a generic module usage, we might need to look up a subnet or VPC.
+  subnet_id              = "subnet-04ac8df91e75657c9" # Optional: specify if needed, otherwise default VPC is often used or it might fail if no default VPC. For a generic module usage, we might need to look up a subnet or VPC.
 
   tags = {
     Terraform   = "true"
