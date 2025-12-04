45b103a2a83ad770142e84b89fb951595c15b2e6
Sujay Samanta <sujay.samanta@hashicorp.com>
2025-12-04 09:42:58 +0530

    Consolidate Terraform provider configuration into terraform.tf and remove versions.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "~>1.13.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
   cloud {
     organization = "sujay-test-01"
     workspaces {
