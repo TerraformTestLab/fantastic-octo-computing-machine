667d492b6db19c767b8c2038e29f93880dbc489f
Sujay Samanta <sujay.samanta@hashicorp.com>
2026-01-12 08:49:02 +0530

    Add Terraform configuration files for stack migration and AWS resources

---
+++ stacks_migration_infra/variables.tf
@@ -0,0 +1,30 @@
+variable "organization_name" {
+  default = "sujay-test-01"
+}
+
+variable "tfe_hostname" {
+  default = "app.terraform.io"
+}
+
+variable "project_name" {
+  default = "stack-with-source"
+}
+
+variable "stack_name" {
+  default = "stack-with-migration-source-v1"
+}
+
+variable "stacks_config_file_dir" {
+  default = "/Users/sujaysamanta/Workspace/Cursor-AI/terraform-stacks-migration-test/stack-terraform-test-v4/fantastic-octo-computing-machine/_stacks_generated"
+}
+
+variable "terraform_config_dir" {
+  default = "/Users/sujaysamanta/Workspace/Cursor-AI/terraform-stacks-migration-test/stack-terraform-test-v4/fantastic-octo-computing-machine"
+}
+
+variable "workspace_deployment_mapping" {
+  default = {
+    dev-workspace  = "dev-workspace"
+    prod-workspace = "prod-workspace"
+  }
+}
