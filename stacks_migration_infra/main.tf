fe60bf15c1ebd4a99870386e73183ee120fddc8c
Sujay Samanta <sujay.samanta@hashicorp.com>
2026-01-22 02:14:11 +0530

    Add Terraform configuration files for stack migration setup

---
+++ stacks_migration_infra/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = ">= 0.73.0"
+    }
+    tfmigrate = {
+      source  = "hashicorp/tfmigrate"
+      version = "2.0.0-beta1"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname     = var.tfe_hostname
+  organization = var.organization_name
+}
+
+resource "tfe_project" "stack_project" {
+  name         = var.project_name
+  organization = var.organization_name
+}
+
+resource "tfe_stack" "stack" {
+  name       = var.stack_name
+  project_id = tfe_project.stack_project.id
+  migration  = true # Identifies the stack is created for migration purposes
+}
+
+resource "tfmigrate_stack_migration" "stack_migration" {
+  config_file_dir              = var.stacks_config_file_dir
+  organization                 = var.organization_name
+  name                         = tfe_stack.stack.name
+  project                      = tfe_project.stack_project.name
+  terraform_config_dir         = var.terraform_config_dir
+  workspace_deployment_mapping = var.workspace_deployment_mapping
+}
