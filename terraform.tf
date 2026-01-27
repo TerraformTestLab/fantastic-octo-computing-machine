6547b9fd2e70837cc27e1b70720a85e0163f2206
Sujay Samanta <sujay.samanta@hashicorp.com>
2026-01-27 11:17:09 +0530

    COMMIT_MSG

--- terraform.tf
+++ terraform.tf
@@ -9,9 +9,10 @@ terraform {
   cloud {
     organization = "sujay-test-01"
     workspaces {
-      project = "Tf-Migrate-Stack-Migration-Retry-Test"
+      # project = "Tf-Migrate-Stack-Migration-Retry-Test"
       tags = {
-        "terraform-migrate-stack-migration-retry-test" = "true"
+        # "terraform-migrate-stack-migration-retry-test" = "true"
+        "migration-test" = "true"
       }
     }
   }
